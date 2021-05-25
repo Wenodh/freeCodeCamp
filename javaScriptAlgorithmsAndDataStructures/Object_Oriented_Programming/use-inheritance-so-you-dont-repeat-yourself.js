c75785c940299c6709863cba9fa219ef410d441a
Wenodh <vinodkumara3@gmail.com>
2021-05-25 22:49:43 +0530

    added use-inheritance-so-you-dont-repeat-yourself.js

---
+++ javaScriptAlgorithmsAndDataStructures/Object_Oriented_Programming/use-inheritance-so-you-dont-repeat-yourself.js
@@ -0,0 +1,19 @@
+function Cat(name) {
+  this.name = name;
+}
+
+
+function Bear(name) {
+  this.name = name;
+}
+
+function Animal() { };
+
+Animal.prototype = {
+  constructor: Animal, 
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+let a = new Cat
+console.log(a.eat)
