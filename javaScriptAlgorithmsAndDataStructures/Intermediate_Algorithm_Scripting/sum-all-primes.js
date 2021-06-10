f897940a767084f923c18573dbb3c9fa7168a825
Wenodh <vinodkumara3@gmail.com>
2021-06-10 15:21:18 +0530

    added sum-all-primes.js

---
+++ javaScriptAlgorithmsAndDataStructures/Intermediate_Algorithm_Scripting/sum-all-primes.js
@@ -0,0 +1,16 @@
+function sumPrimes(num) {
+  let sum = 0;
+  for (let i = 2; i <= num; i++) {
+    if (isPrime(i))
+      sum += i;
+  }
+  return sum;
+}
+function isPrime(num) {
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i == 0)
+      return false;
+  }
+  return true;
+}
+console.log(sumPrimes(10));
